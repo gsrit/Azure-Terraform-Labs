14bd03594f74135b626a3bd79494f4ec084906eb
Gaurav Singh <gauravit@live.com>
2022-07-19 18:55:56 +0530

    Push-Changes

--- 01-az-create-rsg/output.tf
+++ 01-az-create-rsg/output.tf
@@ -1,3 +1,7 @@
 output "resource_group_name" {
-    value = azurerm_resource_group.rg.name
+    value = "azurerm_resource_group.location"
 }
+
+
+
+#value = azurerm_resource_group.rg.name
--- 01-az-create-rsg/variables.tf
+++ 01-az-create-rsg/variables.tf
@@ -1,9 +1,10 @@
+## TEST_Git
 variable "resource_group_name_prefix" {
   default       = "rg"
   description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "resource_group_location" {
-  default = "eastus"
+  default = "southindia"
   description   = "Location of the resource group."
 }
