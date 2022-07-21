8d237ff7331e10467ebd32f0f74c74b4229c73dc
Gaurav Singh <gauravit@live.com>
2022-07-21 10:49:49 +0530

    rsg -changes

--- 01-az-create-rsg/main.tf
+++ 01-az-create-rsg/main.tf
@@ -1,16 +1,13 @@
 #https://docs.microsoft.com/en-us/azure/developer/terraform/create-resource-group?tabs=azure-cli
 
+
+
+
+
+
 resource "azurerm_resource_group" "rsg-prd001" {
   name      = "gslab-rsg001"
   location  = "southindia"
 }
 
 
-#resource "random_pet" "rg-name" {
-#  prefix    = var.resource_group_name_prefix
-#}
-
-#resource "azurerm_resource_group" "rg" {
-#  name      = random_pet.rg-name.id
-#  location  = var.resource_group_location
-#}
