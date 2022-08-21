6b91c6c89cfbc4e93dc6a4d01974dea0c50deaf3
Gaurav Singh <gauravit@live.com>
2022-08-21 21:17:04 +0530

    removed vm

--- 12-az-loop-vm/variables.tf
+++ 12-az-loop-vm/variables.tf
@@ -1,7 +1,7 @@
 variable "vm_name" {
 description         = "Name of the VM."
 type                = list(string)
-default             = ["labvmtesta", "labvmtestb" ,"labvmtestc", "labvmtestd", "labvmteste", "labvmtestf", "labvmtestm"]
+default             = ["labvmtesta", "labvmtestb" ,"labvmtestc" ,"labvmtestc" ,"labvmtestc"]
 }
 
 variable "location" {
